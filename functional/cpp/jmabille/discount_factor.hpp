14ab338944cdf36e3b29ba1341c3f9396b04635a
Johan Mabille <johan.mabille@gmail.com>
2017-11-05 11:50:34 +0100

    typo fix

---
+++ functional/cpp/jmabille/discount_factor.hpp
@@ -0,0 +1,5 @@
+#ifndef DISCOUNT_FACTOR_HPP
+#define DISCOUNT_FACTOR_HPP
+    double discount_factor(double rate, double maturity);
+    void print_discount_factor(double rate, double maturity);
+#endif
