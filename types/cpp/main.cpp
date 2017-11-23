2f318d4aa56d3b51f59804303bfde3d4c4a53fe2
Johan Mabille <johan.mabille@gmail.com>
2017-11-23 10:57:04 +0100

    cpp folder

---
+++ types/cpp/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "matrix.hpp"
+#include "uvector.hpp"
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
+
---
+++ types/cpp/matrix.hpp
@@ -0,0 +1,11 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include <vector>
+
+namespace dauphine
+{
+}
+
+#endif
+
