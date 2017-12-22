96d29f7fce70eabf4e7be68aee85f6f3c31eedd5
=SJD1882 <sjd1882@gmail.com>
2017-12-22 23:37:19 +0100

    Created a starting base for the C++ Project

---
+++ projects/CPP_SCORING_FX/ScoringFX/ScoringFX/main.cpp
@@ -0,0 +1,34 @@
+/*
+CURRENCY RANKING C++ MASTER 203 PROJECT
+- Sebastien DAVID
+- Julien LEROY
+- Thomas FOURCROY
+
+Date: 22/12/2017
+File: Implementation
+*/
+
+#include <iostream>
+#include "scoring.h"
+#include "scoring.cpp"
+
+
+int main() {
+	
+	std::cout << "Zero Point Energy Field Manipulator" << std::endl;
+
+	/*
+	Part 1: IMPLEMENTATION WITH RANDOMIZED DATA
+	*/
+	
+	// [To be completed]
+
+	/*
+	Part 2: IMPLEMENTATION WITH 25D FX RISK REVERSAL DATA
+	*/
+
+	// [To be completed]
+	
+	system("pause");
+	return 0;
+}
---
+++ projects/CPP_SCORING_FX/ScoringFX/ScoringFX/scoring.cpp
@@ -0,0 +1,11 @@
+/*
+CURRENCY RANKING C++ MASTER 203 PROJECT
+- Sebastien DAVID
+- Julien LEROY
+- Thomas FOURCROY
+
+Date: 22/12/2017
+File: FX Scoring Class Methods
+*/
+
+#include "scoring.h"
