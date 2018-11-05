b8537bef493838a3864c30aee8ac70cee7bc9364
Johan Mabille <johan.mabille@gmail.com>
2018-11-05 21:53:39 +0100

    matrix class skeleton

--- types/cpp/matrix.hpp
+++ types/cpp/matrix.hpp
@@ -1,10 +1,23 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+#include <cstddef>
 #include <vector>
 
 namespace dauphine
 {
+    class matrix
+    {
+    public:
+
+        matrix(std::size_t nb_rows, std::size_t nb_cols);
+
+    private:
+
+        std::size_t m_nb_rows;
+        std::size_t m_nb_cols;
+        std::vector<double> m_data;
+    };
 }
 
 #endif
