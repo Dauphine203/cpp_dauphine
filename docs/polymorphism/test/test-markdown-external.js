05a6b15fc2edfbb342fd00301428cbc99bc9a92b
Johan Mabille <johan.mabille@gmail.com>
2018-11-21 14:32:05 +0100

    slides online

--- docs/polymorphism/test/test-markdown-external.js
+++ docs/polymorphism/test/test-markdown-external.js
@@ -1,20 +1,24 @@
+
+
 Reveal.addEventListener( 'ready', function() {
 
 	QUnit.module( 'Markdown' );
 
-	QUnit.test( 'Vertical separator', function( assert ) {
-		assert.strictEqual( document.querySelectorAll( '.reveal .slides>section>section' ).length, 2, 'found two slides' );
+	test( 'Vertical separator', function() {
+		strictEqual( document.querySelectorAll( '.reveal .slides>section>section' ).length, 2, 'found two slides' );
 	});
 
-	QUnit.test( 'Horizontal separator', function( assert ) {
-		assert.strictEqual( document.querySelectorAll( '.reveal .slides>section' ).length, 2, 'found two slides' );
+	test( 'Horizontal separator', function() {
+		strictEqual( document.querySelectorAll( '.reveal .slides>section' ).length, 2, 'found two slides' );
 	});
 
-	QUnit.test( 'Language highlighter', function( assert ) {
-		assert.strictEqual( document.querySelectorAll( '.hljs-keyword' ).length, 1, 'got rendered highlight tag.' );
-		assert.strictEqual( document.querySelector( '.hljs-keyword' ).innerHTML, 'var', 'the same keyword: var.' );
+	test( 'Language highlighter', function() {
+		strictEqual( document.querySelectorAll( '.hljs-keyword' ).length, 1, 'got rendered highlight tag.' );
+		strictEqual( document.querySelector( '.hljs-keyword' ).innerHTML, 'var', 'the same keyword: var.' );
 	});
+
 
 } );
 
 Reveal.initialize();
+
